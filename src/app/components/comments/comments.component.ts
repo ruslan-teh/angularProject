ca943c15daf6a91e0681bd551f44bb9d74da1bbc
ruslana380900 <ruslanachernenko94@gmail.com>
2022-05-19 17:25:13 +0300

    angularProject

---
+++ src/app/components/comments/comments.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {IComment} from "../../models";
+import {CommentService} from "../../services/comment.service";
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.css']
+})
+export class CommentsComponent implements OnInit {
+
+  comments: IComment[];
+
+  constructor(private commentService: CommentService) {
+
+  }
+
+  ngOnInit(): void {
+    this.commentService.getAll().subscribe(value => this.comments = value);
+  }
+
+}
---
+++ src/app/constans/urls.ts
@@ -0,0 +1,7 @@
+const  baseURL ='https://jsonplaceholder.typicode.com';
+
+export const urls = {
+  users: `${baseURL}/users`,
+  posts: `${baseURL}/posts`,
+  comments: `${baseURL}/comments`,
+}
